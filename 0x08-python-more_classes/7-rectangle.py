5c1940e691c382f9b3ec77130c40cb0756fe39da
Kwendo-K <kevinkwendo95@gmail.com>
2021-10-06 00:19:22 +0300

    7. Change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -84,7 +84,7 @@ class Rectangle():
         """ Prints rectangle with #'s """
         if self.__width == 0 or self.__height == 0:
             return ""
-        r = "\n".join([str(self.print_symbol)*width for rows in range(height)])
+        r = "\n".join([str(self.print_symbol)*self.width for rows in range(self.height)])
         return r
 
     def __repr__(self):
