cd534b358f1849b2990ffa14eb485bceae426ba3
Kwendo-K <kevinkwendo95@gmail.com>
2021-10-13 12:53:46 +0300

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ args: filename, mode, encoding
 def read_file(filename=""):
     with open(filename, mode='r', encoding='utf-8') as f:
         data = f.read()
-        print(data)
+        print(data, end="")
