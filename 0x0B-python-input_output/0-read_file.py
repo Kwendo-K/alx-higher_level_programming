50ecf3f1f1a6fbaee2996b28c06dcd7860d685ff
Kwendo-K <kevinkwendo95@gmail.com>
2021-10-13 12:30:37 +0300

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+this functions reads contents from a file
+args: filename
+"""
+
+
 def read_file(filename=""):
     with open(filename, 'r') as f:
         data = f.read()
