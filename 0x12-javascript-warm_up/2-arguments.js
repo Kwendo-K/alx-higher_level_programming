94a7e815265908b8f9832a3bf9fdb790698be554
Kwendo-K <kevinkwendo95@gmail.com>
2021-12-14 23:32:02 +0300

    2. Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-let countArgs = process.argv.length;
+const countArgs = process.argv.length;
 console.log(countArgs === 2 ? 'No argument' : countArgs === 3 ? 'Argument found' : 'Arguments found');
