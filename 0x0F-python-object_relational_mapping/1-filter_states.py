0adcfd4457e92b97e902219fa0e9c70d5e5494b1
Kwendo-K <kevinkwendo95@gmail.com>
2021-12-20 23:02:07 +0300

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import MySQLdb
 
-"""
-Python script that displays state names starting with N
-"""
-#creating mysql connection
+# Python script that displays state names starting with N
+# Myconn is a varible that stores mysql connection
+#
+
 myconn = MySQLdb.connect(host="localhost", user="root", passwd="Asila@1991",
-                         db="hbtn_0e_0_usa")
+                         port=3306, db="hbtn_0e_0_usa")
 cur = myconn.cursor()
 
 try:
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
 except Exception:
     myconn.rollback()
