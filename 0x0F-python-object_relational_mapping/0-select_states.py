9f21763a0321f8afb87478c8dfba840f9769a0ef
Kwendo-K <kevinkwendo95@gmail.com>
2021-12-21 12:29:14 +0300

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,22 +7,22 @@ from hbtn_0e_0_usa database
 # Open database connection
 myconn = MySQLdb.connect(host="localhost", user="root", passwd="Asila@1991",
                          db="hbtn_0e_0_usa")
+
 # Preparing a cursor object using a cursor method
 cur = myconn.cursor()
-
 # Preparing a query that gets all state names
 sql = """SELECT * FROM states"""
 try:
     # Executing the sql command
     cur.execute(sql)
     # Commiting changes in the database
-    results = cur.fetchall()
+    results = cur.fetchall()# Fetcthing all names in states
+    for row in cur:
+        print(row)
+
 except Exception:
     myconn.rollback()
-
-# Fetcthing all names in states
-for row in cur:
-    print(row)
+    print("Unable to fetch data")
 
 # Closing the database connection
 myconn.close()
