192195d6153e2434985e4e7c75755f8c0eaff2b2
Kwendo-K <kevinkwendo95@gmail.com>
2021-12-21 12:04:53 +0300

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,14 +2,16 @@
 import MySQLdb
 
 """
-A python script that displays all the states
-from hbtn_0e_0_usa
+A python script that displays all state names
+from hbtn_0e_0_usa database
 """
-
+# Open database connection
 myconn = MySQLdb.connect(host="localhost", user="root", passwd="Asila@1991",
                          db="hbtn_0e_0_usa")
+# Preparing a cursor object using a cursor method
 cur = myconn.cursor()
 
+# Executing a query that gets all state names
 try:
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
@@ -19,5 +21,5 @@ except Exception:
 for row in rows:
     print(row)
 
-cur.close()
+# Closing the database connection
 myconn.close()
