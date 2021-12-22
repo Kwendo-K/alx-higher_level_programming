76b4ca78324ce56cd9797b17a59da11b38fb9d81
Kwendo-K <kevinkwendo95@gmail.com>
2021-12-22 11:44:12 +0300

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,24 +5,26 @@ from sys import argv
 A python script that displays all state names
 from hbtn_0e_0_usa database
 """
-# Open database connection
-myconn = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                         port=3306, db=argv[3])
+if __name__ == "__main__":
 
-# Preparing a cursor object using a cursor method
-cur = myconn.cursor()
-# Preparing a query that gets all state names
-sql = """SELECT * FROM states"""
-try:
-    # Executing the sql command
-    cur.execute(sql)
-    # Commiting changes in the database
-    results = cur.fetchall()
-    # Fetcthing all names in states
-    for row in cur:
-        print(row)
-except Exception:
-    print("Unable to fetch data")
+    # Open database connection
+    myconn = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                             port=3306, db=argv[3])
 
-# Closing the database connection
-myconn.close()
+    # Preparing a cursor object using a cursor method
+    cur = myconn.cursor()
+    # Preparing a query that gets all state names
+    sql = """SELECT * FROM states"""
+    try:
+        # Executing the sql command
+        cur.execute(sql)
+        # Commiting changes in the database
+        results = cur.fetchall()
+        # Fetcthing all names in states
+        for row in cur:
+            print(row)
+    except Exception:
+        print("Unable to fetch data")
+
+    # Closing the database connection
+    myconn.close()
